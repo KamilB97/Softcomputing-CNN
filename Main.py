89d0982484fbc88d58ec2998fecf1ba434ae802d
KamilB97 <kbrzycki@gmail.com>
2020-10-26 04:31:13 +0100

    Deleted previous version of code
    Added working image reading, resizing and grayscale.
    Added splitting into train and test data

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import csv
+
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
@@ -5,96 +7,68 @@ import os
 import cv2
 import pandas as pd
 import random
+import time
 
+from PIL import Image, ImageOps
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+import tqdm
+
 
 
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/
 path = "datasets"
 labelFile = 'labels.csv'
 
-epochs_val=10
-imageDimensions=(30,30,3)
-testRatio=0.2
-validationRatio=0.2
+data = []
+labels = []
+classes = 40
 
-count=0
-images=[]
-classNo=[]
-myList = os.listdir(path)
-print("Classes detected: ",len(myList))
-noOfClasses=len(myList)
-print("importing classes........")
-for x in range(0,len(myList)):
-    myPicList = os.listdir(path +"/"+str(count))
-    for y in myPicList:
-        curImg = cv2.imread(path+"/"+str(count)+"/"+y)
-        images.append(curImg)
-        classNo.append(count)  
-    print(count, end =" ")
-    count +=1
-print(" ")
-images = np.array(images)
-classNo = np.array(classNo)
+filename = "labels.csv"
 
-x_train, x_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
-x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size=validationRatio)
+# load dictionary
+# with open(filename, 'r') as data:
+#     for line in csv.reader(data):
+#         print(line)
 
-#x_train is array of images to train and y_train is corresponding class id
+data = []
+labels = []
+classes = 40
 
-print("data shapes")
-print("Train",end =""); print(x_train.shape, y_train.shape)
-print("Validation",end=""); print(x_validation.shape,y_validation.shape)
-print("test", end=""); print(x_test.shape, y_test.shape)
+for i in range(classes):
+    path = os.path.join(os.getcwd(), 'datasets', str(i))
+    images = os.listdir(path)
+    counter = 0;
+    for j in images:
+        try:
+            image = Image.open(path + '\\' + j)
+            image = ImageOps.grayscale(image)
+            image = image.resize((30, 30))
+            # showImageTest, delay 5 sec, brief
+            # counter += 1
+            # if counter%400 ==0:
+                # print("j value: " + j)
+                #
+                # image.show()
+                # time.sleep(5)
 
-#assert(x_train.shape[0]==y_train.shape[0]), "train number of images is not equal to the number of labels in training set"
-#assert(x_validation.shape[0]==y_validation.shape[0]), "the number of images is not eqyaual to the number of labels in validation set"
-#assert(x_test.shape==y_test.shape[0]), "the number of images is not equal to the number of labels in test set"
-#assert(x_train.shape[1:]==(imageDimensions)), "the dimensions of the training images are wrong"
-#assert(x_validation.shape[1:]==(imageDimensions)),"the dimesions of the validation images are wrong"
-#assert(x_test.shape[1:]==(imageDimensions)), "the dimensions of the test images are wrong"
+            image = np.array(image)
+            data.append(image)
+            labels.append(i)
 
-data= pd.read_csv(labelFile, error_bad_lines=False)
-print("data shape"), data.shape, type(data)
+        except:
+            print("Error loading image")
+# Converting lists into numpy arrays
+#
+data = np.array(data)
+labels = np.array(labels)
+print(data.shape, labels.shape)
 
-num_of_samples = []
-cols=5
-num_classes = noOfClasses
-fig,axs = plt.subplots(nrows=num_classes, ncols=cols, figsize=(5,300))
-fig.tight_layout()
-for i in range(cols):
-    for j,row in data.iterrows():
-        x_selected  = x_train[y_train ==j]
-        axs[j][i].imshow(x_selected[random.randint(0,len(x_selected)-1),:,:], cmap=plt.get_camp("gray"))
-        axs[j][i].axis("off")
-        if i == 2:
-            axs[j][i].set_title(str(j)+ "-"+row["Name"])
-            num_of_samples.append(len(x_selected))
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=68)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 
-#samples for each categoriy
-
-print(num_of_samples)
-plt.figure(figsize=(12,4))
-plt.bat(range(0,num_classes), num_of_samples)
-plt.title("distribution of the training dataset")
-plt.xlabel("class number")
-plt.ylabel("number of images")
-plt.show
-
-def grayscale(img):
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    return img
-def equalize(img):
-    img=cv2.equalizeHist(img)
-    return img
-def imagePreprocessing(img):
-    img=grayscale(img)
-    img=equalize(img)
-    img=img/255 #normalize vlues betwween 0 an 1
-
-x_train=np.array(list(map(imagePreprocessing,x_train))) #preprocess all images
-x_test=np.array(list(map(imagePreprocessing,x_test)))
-x_validation=np.array(list(map(imagePreprocessing,x_validation)))
-
-cv2.imshow("Gray image", x_train[random.randint(0, len(x_train)-1)]) #validation if preprocessing is correctly done
